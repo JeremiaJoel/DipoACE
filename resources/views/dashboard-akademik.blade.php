a01429f6c3a248af4ba59bec65e4120b82869791
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-11-07 19:57:22 +0700

    Delete resources directory

--- resources/views/dashboard-akademik.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo"Halo, Welcome di halaman akadmik";
-        echo"<h1>".Auth::user()->email."<h1>";
-        // return view('dashboard');
-        echo "TES PULL PUSH";
-        echo "<a href='logout'> Logout </a>";
--- resources/views/dashboard-pembimbing.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo 'Halo, Welcome di halaman pembimbing Akademik';
-echo '<h1>' . Auth::user()->email . '<h1>';
-
-// return view('dashboard');
-echo "<a href='logout'> Logout </a>";
