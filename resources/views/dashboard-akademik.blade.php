f964cbdacc86dd8b8257481d7dcae26c426a4c87
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-11-07 10:21:20 +0700

    Update dashboard-akademik.blade.php

--- resources/views/dashboard-akademik.blade.php
+++ resources/views/dashboard-akademik.blade.php
@@ -3,4 +3,5 @@
 echo"Halo, Welcome di halaman akadmik";
         echo"<h1>".Auth::user()->email."<h1>";
         // return view('dashboard');
+        echo "TES PULL PUSH";
         echo "<a href='logout'> Logout </a>";
