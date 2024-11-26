9a4ad69b4c4b14d719689657d150d7e7c8c7715b
dzakmu <mehmetdzaky@gmail.com>
2024-11-26 07:33:56 +0700

    p

--- database/seeders/DummyUsersSeeder.php
+++ database/seeders/DummyUsersSeeder.php
@@ -14,24 +14,24 @@ class DummyUsersSeeder extends Seeder
     public function run(): void
     {
         $userData = [
-            // [
-            //     'email' => 'ahmad.maulana@students.dipoace.ac.id',
-            //     'password' => bcrypt('123456'),
-            //     'dekan' => '0',
-            //     'mahasiswa' => '1',
-            //     'pembimbing_akademik' => '0',
-            //     'kaprodi' => '0',
-            //     'bagian_akademik' => '0'
-            // ],
-            // [
-            //     'email' => 'adiwibowo@lecturer.dipoace.ac.id',
-            //     'password' => bcrypt('123456'),
-            //     'dekan' => '1',
-            //     'mahasiswa' => '0',
-            //     'pembimbing_akademik' => '1',
-            //     'kaprodi' => '0',
-            //     'bagian_akademik' => '0'
-            // ],
+            [
+                'email' => 'ahmad.maulana@students.dipoace.ac.id',
+                'password' => bcrypt('123456'),
+                'dekan' => '0',
+                'mahasiswa' => '1',
+                'pembimbing_akademik' => '0',
+                'kaprodi' => '0',
+                'bagian_akademik' => '0'
+            ],
+            [
+                'email' => 'adiwibowo@lecturer.dipoace.ac.id',
+                'password' => bcrypt('123456'),
+                'dekan' => '1',
+                'mahasiswa' => '0',
+                'pembimbing_akademik' => '1',
+                'kaprodi' => '0',
+                'bagian_akademik' => '0'
+            ],
             [
                 'email' => 'budibalsem@akademik.dipoace.ac.id',
                 'password' => bcrypt('123456'),
