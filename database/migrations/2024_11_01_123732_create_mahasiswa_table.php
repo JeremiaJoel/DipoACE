1a2c044eebb644f9999c2d4827f8980887566024
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-11-29 15:44:50 +0700

    Add files via upload

---
+++ database/migrations/2024_11_01_123732_create_mahasiswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim')->unique();
+            $table->string('nama');
+            $table->string('jurusan');
+            $table->string('email');
+            $table->string('tanggal_lahir');
+            $table->string('no_hp');
+            $table->string('status');
+            $table->string('pembimbing_akademik');
+            $table->foreignId('doswal_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
---
+++ database/migrations/2024_11_27_175644_create_jadwal_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->id();
+            $table->string('ruang');
+            $table->string('matakuliah');
+            $table->string('waktu');
+            $table->string('kelas');
+            $table->string('semester_aktif');
+            $table->string('jurusan');
+            $table->string('pengampu_1')->default('');
+            $table->string('pengampu_2')->default('');
+            $table->string('pengampu_3')->nullable();
+            $table->enum('status', ['Belum Disetujui', 'Sudah Disetujui'])->default('Belum Disetujui');
+            $table->foreignId('matakuliah_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwal');
+    }
+};
