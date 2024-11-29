854b806e47b8928fbccb4f2b2606affc2644748b
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-11-29 15:46:10 +0700

    Update and rename schedule.php to jadwal.php

--- app/Models/jadwal.php
+++ app/Models/jadwal.php
@@ -2,16 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Matakuliah;
+use App\Models\Dosen;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class schedule extends Model
+class Jadwal extends Model
 {
     use HasFactory;
 
-    protected $table = 'schedules';
+    protected $table = 'jadwal';
     protected $fillable = [
-        'dosen', 
         'ruang',
         'matakuliah',
         'waktu',
@@ -22,5 +24,15 @@ class schedule extends Model
         'pengampu_2',
         'pengampu_3'
 
-];
+    ];
+
+    public function matakuliah()
+    {
+        return $this->belongsTo(matakuliah::class);
+    }
+
+    public function dosen()
+    {
+        return $this->belongsToMany(dosen::class, 'dosen_jadwal', 'jadwal_id', 'dosen_id');
+    }
 }
