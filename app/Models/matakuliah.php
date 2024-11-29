f0a5fdf357b20bef9d44de3a61d5ee56f84a1ecc
JeremiaJoel <160476257+JeremiaJoel@users.noreply.github.com>
2024-11-29 15:47:11 +0700

    Update matakuliah.php

--- app/Models/matakuliah.php
+++ app/Models/matakuliah.php
@@ -2,18 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Jadwal;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class matakuliah extends Model
+class Matakuliah extends Model
 {
     use HasFactory;
 
+    protected $table = 'matakuliah';
     protected $fillable = [
-        'nama', 
+        'nama',
+        'jurusan',
         'kodemk',
         'sks',
         'semester',
         'jenis_matkul'
     ];
+
+    public function schedules()
+    {
+        return $this->hasMany(Jadwal::class);
+    }
 }
